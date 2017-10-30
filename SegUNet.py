6bd4715b73906e5d5e4b6a1fae9fa043ce0803f3
ykamikawa <ytkmkw@gmail.com>
2017-10-30 20:39:24 +0900

    Added callbacks

--- SegUNet.py
+++ SegUNet.py
@@ -251,7 +251,7 @@ if __name__ == "__main__":
 
         # set callbacks
         fpath = '../LIP/pretrained/LIP_SegUNet{epoch:02d}.hdf5'
-        cp_cb = ModelCheckpoint(filepath = fpath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+        cp_cb = ModelCheckpoint(filepath = fpath, monitor='val_loss', verbose=1, save_best_only=True, mode='auto', period=10)
         es_cb = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')
         tb_cb = TensorBoard(log_dir="../LIP/pretrained")
 
